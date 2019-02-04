50f2f7def18f7fb982a4958893e81f9ed0ac7f85
Bennimaru <bennywhyu@gamil.com>
2019-02-04 17:11:00 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers,revenue){
+  return drivers.filter(function(driver){
+    return driver.revenue > revenue
+  })
+}
+function driverNamesWithRevenueOver(drivers,revenue){
+  return drivers.filter(function(driver){
+    return driver.revenue > revenue
+  }).map(function(richDriver){
+    return richDriver.name
+  })
+}
+function exactMatch(drivers,object){
+  return drivers.filter(function(driver){
+    let result = false
+    for(const key in object){
+      result = (driver[key]===object[key])
+    }
+    return result
+  })
+}
+function exactMatchToList(drivers,object){
+  return exactMatch(drivers,object)
+    .map(function(driver){
+      return driver.name
+    })
+}
